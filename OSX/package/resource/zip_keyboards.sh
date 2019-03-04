3221384b9bdddbae290ee345a1d0405d5f199800
Thant Thet <t@ttkz.me>
2019-03-05 01:13:59 +0630

    pack Pyidaungsu MM instead of mm3

--- OSX/package/resource/zip_keyboards.sh
+++ OSX/package/resource/zip_keyboards.sh
@@ -3,4 +3,4 @@ cd `dirname $0`
 mkdir -p "others/"
 rm "others/kbLayouts.zip"
 cd ./../../../LayoutScripts/
-zip ./../OSX/package/others/kbLayouts "Zawgyi L.km2" "Zawgyi L - Unicode.km2" "mm3std.km2" "PaOh.km2" "Yunghkio SHAN.km2"
+zip ./../OSX/package/resource/others/kbLayouts "Zawgyi L.km2" "Zawgyi L - Unicode.km2" "Pyidaungsu MM.km2" "PaOh.km2" "Yunghkio SHAN.km2"
