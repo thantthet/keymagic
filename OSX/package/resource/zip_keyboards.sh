cb566b5cab0e7271283deeafa2cfd06f217c6757
Thant Thet <t@ttkz.me>
2019-09-23 16:33:05 +0630

    [macos] flatten when zipping keyboards

--- OSX/package/resource/zip_keyboards.sh
+++ OSX/package/resource/zip_keyboards.sh
@@ -5,4 +5,4 @@ rm "others/kbLayouts.zip"
 # assuming keymagic repo and keymagic-keyboards repo are in the same directory
 # so you need to clone keymagic-keyboards repo to run successfully
 cd ./../../../../keymagic-keyboards/
-zip ./../keymagic/OSX/package/resource/others/kbLayouts "Zawgyi/Zawgyi.km2" "ZawCode/ZawCode.km2" "Pyidaungsu/Pyidaungsu MM.km2" "PaOh/PaOh.km2" "Yunghkio/Yunghkio SHAN.km2"
+zip --junk-paths ./../keymagic/OSX/package/resource/others/kbLayouts "Zawgyi/Zawgyi.km2" "ZawCode/ZawCode.km2" "Pyidaungsu/Pyidaungsu MM.km2" "PaOh/PaOh.km2" "Yunghkio/Yunghkio SHAN.km2"
