bb016af9707a31be00e9e9fdd3c224084c05002a
Thant Thet <t@ttkz.me>
2019-09-23 16:06:37 +0630

    [update-s3-version.sh] echo no new line

--- OSX/IMKBased/update-s3-version.sh
+++ OSX/IMKBased/update-s3-version.sh
@@ -6,6 +6,6 @@ fi
 
 filename=$(mktemp)
 
-echo "$1" > ${filename}
+echo -n "$1" > ${filename}
 aws s3 cp ${filename} s3://keymagic/releases/macos/latest/version.txt --acl public-read --profile thantthet	
 rm ${filename}
