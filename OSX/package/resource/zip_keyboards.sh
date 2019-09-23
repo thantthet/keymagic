997e02942897f312a5698c6bcce3fca635acd86a
Thant Thet <t@ttkz.me>
2019-09-23 16:23:59 +0630

    [macos] update zip_keyboards.sh

--- OSX/package/resource/zip_keyboards.sh
+++ OSX/package/resource/zip_keyboards.sh
@@ -2,5 +2,7 @@
 cd `dirname $0`
 mkdir -p "others/"
 rm "others/kbLayouts.zip"
-cd ./../../../LayoutScripts/
-zip ./../OSX/package/resource/others/kbLayouts "Zawgyi L.km2" "Zawgyi L - Unicode.km2" "Pyidaungsu MM.km2" "PaOh.km2" "Yunghkio SHAN.km2"
+# assuming keymagic repo and keymagic-keyboards repo are in the same directory
+# so you need to clone keymagic-keyboards repo to run successfully
+cd ./../../../../keymagic-keyboards/
+zip ./../keymagic/OSX/package/resource/others/kbLayouts "Zawgyi/Zawgyi.km2" "ZawCode/ZawCode.km2" "Pyidaungsu/Pyidaungsu MM.km2" "PaOh/PaOh.km2" "Yunghkio/Yunghkio SHAN.km2"
