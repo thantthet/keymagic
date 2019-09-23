1d6476f4f7b43608d0dbd792a2bb61d3e7cb03a3
Thant Thet <t@ttkz.me>
2019-09-23 15:54:41 +0630

    [macos] change s3 bucket

--- OSX/IMKBased/update-s3-version.sh
+++ OSX/IMKBased/update-s3-version.sh
@@ -7,5 +7,5 @@ fi
 filename=$(mktemp)
 
 echo "$1" > ${filename}
-aws s3 cp ${filename} s3://storage.keymagic.net/releases/macos/latest/version.txt --acl public-read --profile thantthet	
+aws s3 cp ${filename} s3://keymagic/releases/macos/latest/version.txt --acl public-read --profile thantthet	
 rm ${filename}
