4be5a568d293fbf81f1d310a924a949d13d731b6
Thant Thet <t@ttkz.me>
2020-01-25 10:50:09 +0630

    update build-release-vs2019.bat

--- windows/KeyMagic2/build-release-vs2019.bat
+++ windows/KeyMagic2/build-release-vs2019.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsMSBuildCmd.bat"
-msbuild KeyMagic2.sln /t:Build /p:Configuration=Release
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+msbuild KeyMagic2.sln /t:Build /p:Configuration=Release /p:Platform=x86
 copy /B ".\x64\Release\KeyMagic2.exe" ".\..\inno\KeyMagic2\KeyMagic2.exe"
 pause
