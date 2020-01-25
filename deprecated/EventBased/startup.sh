60fd54982edf27cccaa6b83401104887ad51c956
Thant Thet <t@ttkz.me>
2020-01-25 11:32:07 +0630

    remove deprecated directory

--- deprecated/EventBased/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-prog=(/Users/thantthetkz/KeyMagic/build/Release/KeyMagic.app/Contents/MacOS/KeyMagic)
-
-killall ${prog[0]##*/}
-
-exec "${prog[@]}" startup
-
-# defaults write com.apple.loginwindow LoginHook /Users/thantthetkz/KeyMagic/startup.sh
