6748aea0ffca11d7a8df25144dbadb83a8de08d2
Thant Thet <t@ttkz.me>
2020-01-27 21:26:41 +0630

    update macos package makefile

--- macOS/package/resource/zip_keyboards.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-mkdir -p "others/"
-rm "others/kbLayouts.zip"
-# assuming keymagic repo and keymagic-keyboards repo are in the same directory
-# so you need to clone keymagic-keyboards repo to run successfully
-cd ./../../../../keymagic-keyboards/
-zip --junk-paths ./../keymagic/OSX/package/resource/others/kbLayouts "Zawgyi/Zawgyi.km2" "ZawCode/ZawCode.km2" "Pyidaungsu/Pyidaungsu MM.km2" "PaOh/PaOh.km2" "Yunghkio/Yunghkio SHAN.km2"
